08616693457f49dc44253e5ad64b7f554b3b5861
Marlon Regis <marlonregis14@outlook.com>
2021-07-18 18:30:50 -0300

    modified person to abstract class and deleted class TestePessoa

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,26 +1,6 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome:String = "Marlon"
-
-    var cpf:String = "122.918.324-84"
-    private set
-
-    inner class Endereco{
-        var rua = "Rua teste"
-    }
-
-    constructor()
-
-    fun unirNomeCpf() = "Nome: $nome , CPF: $cpf"
-}
-
-fun main(){
-    val marlon: Pessoa = Pessoa()
-
-    println(marlon.nome)
-    println(marlon.cpf)
-
-    println(marlon.Endereco().rua)
-    println(marlon.unirNomeCpf())
-}
+abstract class Pessoa(
+   open val nome:String,
+   open val cpf:String
+)
